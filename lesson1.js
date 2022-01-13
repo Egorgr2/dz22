18175548bf6bb6fee14d547f11c1542c9fc4b78d
Egorgr2 <91964708+Egorgr2@users.noreply.github.com>
2022-01-13 18:06:23 +0200

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,37 @@
+const name = prompt("Введите свое имя");
+
+console.log(name);
+
+const ageMy =11;
+const ageFreind =12;
+const age = prompt(ageMy + "+" + ageFreind + "=");
+
+alert('Ваш ответ:' + age)
+
+console.log(age);
+
+const a =43;
+const b =12;
+
+console.log(a+b);
+
+let a1 = 5+3;
+let a2 = 5-3;
+let a3 = 5*3;
+let a4 = 5/3;
+let a5 = 3/5;
+
+console.log(a1, a2, a3, a4, a5);
+
+let a6 = 5%3;
+let a7 = 3%5;
+let a8 = 5+'3';
+let a9 = '5'+3;
+let a10 = 75+'кг';
+// const a11 = prompt(a6 + a7 + a8 + a9 + a10)
+alert('Ответы:' + a6 + ', ' + a7 + ', ' + a8 + ', ' + a9 + ', ' + a10)
+
+let height =23;
+let width =10;
+
+console.log('23*10=' + height*width);
